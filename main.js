1e896b06b9360bd68c30c08577c8f11fe540458f
josemario234 <AL02963295@tecmilenio.mx>
2026-04-20 17:03:42 -0700

    Modifying the data in main.js file

--- main.js
+++ main.js
@@ -4,20 +4,38 @@ var app = new Vue({
         title: 'Chuck Norris Jokes',
         chuck: [ 
             { 
+               "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
                "value": "Chuck Norris can skydive into outer space." 
             }, 
             { 
+                "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
                 "value": "The chief export of Chuck Norris is pain." 
             }, 
             { 
-                 "value": "Chuck Norris doesn't read books. He stares them down until he gets the information he wants." 
+                "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png", 
+                "value": "Chuck Norris doesn't read books. He stares them down until he gets the information he wants." 
              }, 
              { 
-                 "value": "Time waits for no man. Unless that man is Chuck Norris." 
+                "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png", 
+                "value": "Time waits for no man. Unless that man is Chuck Norris." 
              }, 
              { 
-                 "value": "If you spell Chuck Norris in Scrabble, you win. Forever." 
+                "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png", 
+                "value": "If you spell Chuck Norris in Scrabble, you win. Forever." 
              }, 
         ]
+    },
+    components: {
+        'chuck-card': {
+            props: ['icon_url', 'value'],
+            template: `
+                <div class="card h-100">
+                    <img :src="icon_url" class="card-img-top" alt="Chuck Norris">
+                    <div class="card-body">
+                        <p class= "card-text">{{ value }}</p>
+                    </div>
+                </div>
+            `
+        }
     }
 })
